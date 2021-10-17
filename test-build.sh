a3d9607e01c62130ecfe8d0ac8396c404fbd978b
cslev <cs.lev@gmx.com>
2021-10-17 17:30:43 +0800

    modify test-build.sh to accommodate the extra docker env variables and their handling in docker-entrypoint.sh

--- test-build.sh
+++ test-build.sh
@@ -1,14 +1,24 @@
 rm -rf ./config
 rm -rf ./modules
+rm -rf ./css
+rm -rf ./js
+
 mkdir config
 mkdir modules
+mkdir css
+mkdir js
 
-docker rm -f magic_mirror
-docker build -t mm:latest .
-docker run  -d \
+docker rm -f magicmirror
+docker rmi cslev/magicmirror:latest
+docker build -t cslev/magicmirror:latest .
+docker run  -dit \
 	--publish 80:8080 \
 	--restart always \
 	--volume $PWD/config:/opt/magic_mirror/config \
 	--volume $PWD/modules:/opt/magic_mirror/modules \
-	--name magic_mirror \
-	mm:latest
+	--volume $PWD/css:/opt/magic_mirror/css \
+	--volume $PWD/js:/opt/magic_mirror/js \
+	-e DOCKER_MODULE_INSTALL_GIT="https://github.com/slametps/MMM-Yeelight;https://github.com/Kreshnik/MMM-JokeAPI" \
+	-e DOCKER_MODULE_INSTALL_CMD="npm install"\
+	--name magicmirror \
+	cslev/magicmirror:latest
