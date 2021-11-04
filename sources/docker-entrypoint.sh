5d79451ed847d0d86823697a534b2e0b86df9d5c
cslev <cs.lev@gmx.com>
2021-11-04 10:04:02 +0800

    fix

--- sources/docker-entrypoint.sh
+++ sources/docker-entrypoint.sh
@@ -21,53 +21,57 @@ if [ -f "/opt/magic_mirror/config/config.js.template" ]; then
     envsubst < /opt/magic_mirror/config/config.js.template > /opt/magic_mirror/config/config.js
 fi
 
-magicmirror_root="/opt/magic_mirror"
+### WHILE IT SEEMED TO BE A GOOD IDEA TO HAVE INPUT PARAMS TO INSTALL ADDITIONAL MODULES EASILY
+### IT CAN A HUGE OVERHEAD EVERYTIME WE RESTART THE CONAINER WITH ENV VARIABLES UNCHANGED
+# LET'S COMMENT THEM FOR NOW, BUT LEAVE FOR REFERENCE 
 
-module_deps=$DOCKER_MODULE_ADDITIONAL_DEPS
-module_gits=$DOCKER_MODULE_INSTALL_GIT
-module_install_cmd=$DOCKER_MODULE_INSTALL_CMD
+# magicmirror_root="/opt/magic_mirror"
 
-if [[ ! -z $module_install_cmd ]]
-then
-    module_install_cmd="npm install"
-fi
+# module_deps=$DOCKER_MODULE_ADDITIONAL_DEPS
+# module_gits=$DOCKER_MODULE_INSTALL_GIT
+# module_install_cmd=$DOCKER_MODULE_INSTALL_CMD
 
-#create log file for post install logs
-LOG_FILE="/opt/docker_post_install.log"
-echo "" > $LOG_FILE
-#install additional dependencies if required
-if [ ! -z $module_deps ] #if deps is not empty we install, otherwise don't increase container size
-then
-    echo "--- INSTALLING EXTRA DEPS" >> $LOG_FILE
-    apt-get update >> $LOG_FILE
-    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends $module_deps   >> $LOG_FILE
-fi
+# if [[ ! -z $module_install_cmd ]]
+# then
+#     module_install_cmd="npm install"
+# fi
 
-cd /opt/magic_mirror/modules
-#module separator is ';'
-if [[ ! -z $module_gits ]]
-then
-    echo "INSTALLING EXTRA MM Modules..." >> $LOG_FILE
-    for module in $(echo $module_gits|sed -e "s/;/\n/g")
-    do
-        echo $module >> $LOG_FILE
-         #get last part of the URL for the directories
-        dir=$(echo "${module##*/}") #basically, all chars after the last '/'
-        if [ ! -d $dir ]
-        then
-            git clone $module >> $LOG_FILE
-        fi       
-        cd $dir
-        #we still reinstall it if they exists as this is needed anyway
-        $module_install_cmd >> $LOG_FILE
-        cd /opt/magic_mirror/modules/
-        echo "----" >> $LOG_FILE
-    done
-fi
+# #create log file for post install logs
+# LOG_FILE="/opt/docker_post_install.log"
+# echo "" > $LOG_FILE
+# #install additional dependencies if required
+# if [ ! -z $module_deps ] #if deps is not empty we install, otherwise don't increase container size
+# then
+#     echo "--- INSTALLING EXTRA DEPS" >> $LOG_FILE
+#     apt-get update >> $LOG_FILE
+#     DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends $module_deps   >> $LOG_FILE
+# fi
 
-#failsafe, just in case we did not come back to root
-#otherwise, docker CMD will not succeed
-cd $magicmirror_root
+# cd /opt/magic_mirror/modules
+# #module separator is ';'
+# if [[ ! -z $module_gits ]]
+# then
+#     echo "INSTALLING EXTRA MM Modules..." >> $LOG_FILE
+#     for module in $(echo $module_gits|sed -e "s/;/\n/g")
+#     do
+#         echo $module >> $LOG_FILE
+#          #get last part of the URL for the directories
+#         dir=$(echo "${module##*/}") #basically, all chars after the last '/'
+#         if [ ! -d $dir ]
+#         then
+#             git clone $module >> $LOG_FILE
+#         fi       
+#         cd $dir
+#         #we still reinstall it if they exists as this is needed anyway
+#         $module_install_cmd >> $LOG_FILE
+#         cd /opt/magic_mirror/modules/
+#         echo "----" >> $LOG_FILE
+#     done
+# fi
+
+# #failsafe, just in case we did not come back to root
+# #otherwise, docker CMD will not succeed
+# cd $magicmirror_root
 echo "[DONE]"
 
 
